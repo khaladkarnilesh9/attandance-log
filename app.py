ec87b70c9aff585d2afba1bb3dfdb00493800364
khaladkarnilesh9 <48516597+khaladkarnilesh9@users.noreply.github.com>
2025-05-23 15:21:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -152,8 +152,8 @@ st.markdown(html_css, unsafe_allow_html=True)
 
 # --- Credentials ---
 USERS = {
-    "Geetali": {"password": "emp123", "role": "employee"},
-    "Nilesh": {"password": "emp456", "role": "employee"},
+    "Geetali": {"password": "Geetali123", "role": "employee"},
+    "Nilesh": {"password": "Nilesh123", "role": "employee"},
     "admin": {"password": "admin123", "role": "admin"}
 }
 
